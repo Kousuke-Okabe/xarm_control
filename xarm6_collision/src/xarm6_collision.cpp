f03e9fca0b19031c01c4f8db4beada0f0a564374
Kousukbe Okabe <kousuke.okabe@ieee.org>
2024-07-30 18:28:33 +0900

    Check subscrive

--- xarm6_collision/src/xarm6_collision.cpp
+++ xarm6_collision/src/xarm6_collision.cpp
@@ -1,17 +1,23 @@
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
-#include <Engen/Dense>
+#include <mutex>
+#include <Eigen/Dense>
 
 #define DOF 6
-#define Fs 10;
+#define Fs 10
 
-Eigen::Vector6d present_angle;
-Eigen::Vector6d present_torque;
+static std::mutex g_mutex;
+
+Eigen::VectorXd present_angle(6);
+Eigen::VectorXd present_torque(6);
 
 // Callback function of the Topic communication with joint_states
 void Subscribe_JointStates(const sensor_msgs::JointState &state){
+    // lock the mutex
+    std::lock_guard<std::mutex> lock(g_mutex);
+
     // ROS_INFO("Joint Effort : %f", state.effort[1]);
-    for(int i=0; i<DOF; i++){]
+    for(int i=0; i<DOF; i++){
         present_angle(i) = (double)state.position[i];
         present_torque(i) = (double)state.effort[i];
     }
@@ -26,13 +32,17 @@ int main(int argc, char **argv){
     ros::Subscriber sub = nh.subscribe("/xarm/joint_states", 10, Subscribe_JointStates);
 
     ros::Rate rate(Fs);
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
 
     while(ros::ok()){
-        ros::spinOnce();
+        // ros::spinOnce();
+        g_mutex.lock();
 
-        ROS_INFO("effort_0 : %f", present_torque(0));
+        ROS_INFO("effort_0 : %f", present_torque(1));
 
-        rate::sleep();
+        g_mutex.unlock();
+        rate.sleep();
     }
 
     // ros::spin();
